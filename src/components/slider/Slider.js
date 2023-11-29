81bc4717be082b2f9c20aecf9417328e5c9a048f
nalbandyan <nalbandyan.inbox@gmail.com>
2023-11-30 00:04:09 +0400

    End project!

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,4 +1,4 @@
-import React, { memo, useState, useEffect, lazy, useCallback } from "react";
+import React, { useState, useEffect, lazy } from "react";
 import styles from "./Slider.module.scss";
 import "react-range-slider-input/dist/style.css";
 
@@ -14,23 +14,22 @@ const Slider = ({
     sliderMaxValue,
   ]);
 
-  const resetState = () => {
-    setRangeValue([sliderMinValue, sliderMaxValue]);
-  };
-
   useEffect(() => {
+    const resetState = () => {
+      setRangeValue([sliderMinValue, sliderMaxValue]);
+    };
+
     resetState();
-
     return callback();
-  }, [reset, callback]);
+  }, [reset, callback, sliderMinValue, sliderMaxValue]);
 
-  const handlePriceInputChange = useCallback((selector, value) => {
+  const handlePriceInputChange = (selector, value) => {
     setRangeValue((prevRangeValue) => {
       return selector === "min"
         ? [+value, prevRangeValue[1]]
         : [prevRangeValue[0], +value];
     });
-  });
+  };
 
   const RangeMinMax = () => (
     <div className={styles.range__container__values}>
@@ -43,58 +42,58 @@ const Slider = ({
     </div>
   );
 
-  const MinMaxContainer = memo(() => (
-    <div className={styles.range__container__price}>
-      <label className={styles.range__container__price__container}>
-        <legend className={styles.range__container__price__container__currency}>
-          {sliderValuePrefix}
-        </legend>
-        <input
-          value={rangeValue[0]}
-          className={styles.range__container__price__input}
-          type="text"
-          id="min__price"
-          aria-label="Minimum Price"
-          onChange={(event) =>
-            handlePriceInputChange(
-              "min",
-              event.target.value.replace(/[^0-9]/g, "")
-            )
-          }
-        />
-      </label>
-      <span
-        aria-hidden="true"
-        className={styles.range__container__price__container__center__line}
-      >
-        -
-      </span>
-      <label className={styles.range__container__price__container}>
-        <span className={styles.range__container__price__container__currency}>
-          {sliderValuePrefix}
-        </span>
-        <input
-          value={rangeValue[1]}
-          className={styles.range__container__price__input}
-          type="text"
-          id="max__price"
-          aria-label="Maximum Price"
-          onChange={(event) =>
-            handlePriceInputChange(
-              "max",
-              event.target.value.replace(/[^0-9]/g, "")
-            )
-          }
-        />
-      </label>
-    </div>
-  ));
-
   return (
     <details open className={styles.range__container}>
       <summary>{displayName}</summary>
       <section aria-label="brands-heading">
-        <MinMaxContainer />
+        <div className={styles.range__container__price}>
+          <label className={styles.range__container__price__container}>
+            <legend
+              className={styles.range__container__price__container__currency}
+            >
+              {sliderValuePrefix}
+            </legend>
+            <input
+              value={rangeValue[0]}
+              className={styles.range__container__price__input}
+              type="text"
+              id="min__price"
+              aria-label="Minimum Price"
+              onChange={(event) =>
+                handlePriceInputChange(
+                  "min",
+                  event.target.value.replace(/[^0-9]/g, "")
+                )
+              }
+            />
+          </label>
+          <span
+            aria-hidden="true"
+            className={styles.range__container__price__container__center__line}
+          >
+            -
+          </span>
+          <label className={styles.range__container__price__container}>
+            <span
+              className={styles.range__container__price__container__currency}
+            >
+              {sliderValuePrefix}
+            </span>
+            <input
+              value={rangeValue[1]}
+              className={styles.range__container__price__input}
+              type="text"
+              id="max__price"
+              aria-label="Maximum Price"
+              onChange={(event) =>
+                handlePriceInputChange(
+                  "max",
+                  event.target.value.replace(/[^0-9]/g, "")
+                )
+              }
+            />
+          </label>
+        </div>
         <RangeSlider
           min={sliderMinValue}
           max={sliderMaxValue}
